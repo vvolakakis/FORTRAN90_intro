4fad8295844c2e30f22ca96b778e91629a4ff4c3
vvolakakis <104544711+vvolakakis@users.noreply.github.com>
2024-01-30 17:01:25 -0600

    Add files via upload

--- modules_simple.f90
+++ modules_simple.f90
@@ -3,15 +3,15 @@ module modulaki !simple way to go into modules. Modules could be compared with f
   !(pardon any vocab mistakes if any)
   private !meaning that it can be used only inside this module and not outside of it
 
-  public :: calculate_boom !this is like global designation of a variable in python
+  public :: calculate_average !this is like global designation of a variable in python
 
-contains
+  contains !used to define internal subprograms, like a subroutine or  a function within the main program or a module
 
   subroutine calculate_average(data, n, boom)
     real(kind=8), intent(in), dimension(:) :: data !here folks we have to declare everything, even the air we breath needs to be declared (like C)
 	!But lets see what the hell line 11 does: 
 	!real(kind=8) declares data as a real variable with 8-byte precision (double precision)
-	!intent(in) basically we are asked the reason of using data (like when you come to the U.S. customs, you are being asked every single thing), and it means that data is an input variable
+	!intent(in) basically we are asked the reason of using data, and it means that data is an input variable
 	!dimension(:) indicates that data is an array with an unspecified size, not fixed at the point of declaration and can be determined later
 	
     integer, intent(in) :: n
@@ -29,15 +29,32 @@ contains
 
 end module modulaki
 
+
 program moduless
   use modulaki
   implicit none
+  
+  integer factorial !need to declare the function's output type
 
-  real(kind=8), dimension(5) :: values = [1.0, 2.0, 3.0, 4.0, 5.0]
+  real(kind=8), dimension(5) :: values = [1.0, 2.0, 3.0, 4.0, 5.0] !we also have to declare the length of our structure from the begining 
   real(kind=8) :: mpam
-
+  integer :: y
+ 
+  print *, "provide the number that you want to calculate its factorial"
+  read *, y
+  print *, "function check", factorial(y)
+  
   call calculate_average(values, 5, mpam)
-
-  print *, "boom:", mpam
-
+  print *, "average value of list:", mpam
 end program moduless
+
+integer function factorial(k) !there is a difference compared to the above function
+	implicit none
+	integer, intent(in) :: k
+	integer :: i, bamboom
+	bamboom = 1
+	do i = 1, k
+		bamboom = bamboom*i
+	end do
+	factorial = bamboom
+end function factorial
